00e160956e9e97d8f85092661a09547749c55e3f
0bie <obie.egwim.git@gmail.com>
2018-10-02 23:49:34 -0700

    Remove component definitions from docs source files

---
+++ src/scripts/data/avatar/index.js
@@ -0,0 +1,10 @@
+export const avatarProps = {
+  firstName: 'joan',
+  lastName: 'holloway'
+};
+
+export const avatarWithImgProps = Object.assign({}, avatarProps, {
+  image: {
+    src: 'assets/avatar/avatar3.jpg'
+  }
+});
